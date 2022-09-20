68b3e021e41fec8703785ab8d6af96a2036cffc7
Tanjeer <tbh.tilok@live.com>
2022-09-20 18:11:23 +0600

    committed

---
+++ src/LeetCode/Palindrom.cs
@@ -0,0 +1,39 @@
+﻿
+namespace LeetCode;
+
+public class Palindrom
+{
+
+    public static bool IsPalindrom(string sequence)
+    {
+        if(string.IsNullOrEmpty(sequence))
+            return false;
+        for (int i = 0; i < sequence.Length/2 + 1; i++)
+        {
+            if(sequence[i] != sequence[sequence.Length - i - 1])
+                return false;
+        }
+
+        return true;
+    }
+
+    public static string LongestPalindrome(string s)
+    {
+        var longestSubSeq=string.Empty;
+        if (s.Length == 1)
+            return s;
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            for (int j = 1; j <= s.Length - i; j++)
+            {
+                var subSeq = s.Substring(i, j);
+                if (IsPalindrom(subSeq))
+                {
+                    longestSubSeq = subSeq.Length > longestSubSeq.Length ? subSeq : longestSubSeq;
+                }
+            }
+        }
+
+        return longestSubSeq;
+    }
+}
